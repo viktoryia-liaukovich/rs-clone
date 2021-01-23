6b8272cc15e3d39f0b93f7e91bb1022cb52d67a6
TatsianaSlapik <slapik.tatiana@gmail.com>
2021-01-24 00:18:33 +0300

    feat: added loader

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,15 +1,16 @@
-import { $, create } from "../utils/utils"
-import config from "../configs/levels";
-import Level from "./Level";
+import { $, create } from '../utils/utils';
+import config from '../configs/levels';
+import Level from './Level';
+import pageLoader from '../components/pageLoader';
 
 export default function Map() {
-  const mapWrapper = create("div");
-  mapWrapper.classList.add("map");
+  const mapWrapper = create('div');
+  mapWrapper.classList.add('map');
 
   Object.values(config).forEach((el, i) => {
-    const marker = create("span");
+    const marker = create('span');
 
-    marker.classList.add("marker");
+    marker.classList.add('marker');
     marker.style.top = `${el.point.y}px`;
     marker.style.left = `${el.point.x}px`;
 
@@ -18,9 +19,9 @@ export default function Map() {
     marker.addEventListener('click', () => {
       $('#root').innerHTML = '';
       Level(config[i + 1]);
-    })
+      $('#root').appendChild(pageLoader());
+    });
   });
-
 
   return mapWrapper;
 }
