62fddb2cce3bbe4fbc7346c7e441b0a46ba5780d
Viktoryia Ventskute <ventskute.vika@gmail.com>
2021-01-26 17:50:28 +0300

    feat: add marker icon and animation

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -2,13 +2,15 @@ import { $, create } from '../utils/utils';
 import config from '../configs/levels';
 import Level from './Level';
 import pageLoader from '../components/pageLoader';
+import markerIcon from '../assets/UI/location-pin.png';
 
 export default function Map() {
   const mapWrapper = create('div');
   mapWrapper.classList.add('map');
 
   Object.values(config).forEach((el, i) => {
-    const marker = create('span');
+    const marker = new Image();
+    marker.src = markerIcon;
 
     marker.classList.add('marker');
     marker.style.top = `${el.point.y}px`;
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,44 +1,43 @@
 import optionsPopup from '../components/optionsPopup';
 import { $, create } from '../utils/utils';
 import { playMenuMusic } from '../utils/music';
-import Map from "./Map";
+import Map from './Map';
 
 const menuConfig = {
-    NEW_GAME: 'New game',
-    CONTINUE: 'Continue',
-    OPTIONS: 'Options',
-    EXIT: 'Exit game',
+  NEW_GAME: 'New game',
+  CONTINUE: 'Continue',
+  OPTIONS: 'Options',
+  EXIT: 'Exit game',
 };
 
 export default function Menu() {
-    const menuWrapper = create('div');
-    menuWrapper.classList.add('menu');
-  
-    const nav = create('nav');
-  
-    Object.values(menuConfig).forEach((el) => {
-      const li = create('li');
-      li.innerText = el;
-      nav.appendChild(li);
-    });
+  const menuWrapper = create('div');
+  menuWrapper.classList.add('menu');
 
-    nav.addEventListener('click', (e) => {
-        playMenuMusic();
-        switch(e.target.innerText) {
-            case menuConfig.NEW_GAME: {
-                $('#root').innerHTML = '';
-                $('#root').appendChild(Map());
-                break;
-            }
-            case menuConfig.OPTIONS: {
-                $('#root').appendChild(optionsPopup());
-                break;
-            }
-        }
-    })
-  
-    menuWrapper.appendChild(nav);
-  
-    return menuWrapper;
-  }
-  
+  const nav = create('nav');
+
+  Object.values(menuConfig).forEach((el) => {
+    const li = create('li');
+    li.innerText = el;
+    nav.appendChild(li);
+  });
+
+  nav.addEventListener('click', (e) => {
+    playMenuMusic();
+    switch (e.target.innerText) {
+    case menuConfig.NEW_GAME: {
+      $('#root').innerHTML = '';
+      $('#root').appendChild(Map());
+      break;
+    }
+    case menuConfig.OPTIONS: {
+      $('#root').appendChild(optionsPopup());
+      break;
+    }
+    }
+  });
+
+  menuWrapper.appendChild(nav);
+
+  return menuWrapper;
+}
