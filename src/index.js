71068196d7108a05cf72d04e1713b97f47710026
Viktoryia Ventskute <45541398+Ventskute@users.noreply.github.com>
2021-01-30 22:08:17 +0300

    fix: load music and sounds, blur (#13)
    * fix: load music and sounds, blur
    
    * fix: default save

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import Menu from './pages/Menu';
 import Level from './pages/Level';
 import config from './configs/levels';
 import startupModal from './components/startupModal';
+import { load, setDefault } from './utils/saveSystem';
+
+!load() && setDefault();
 
 $('#root').appendChild(Menu());
 $('#root').appendChild(startupModal());
