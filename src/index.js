759bbe4cffa9ad69c2193f7dae58c2e31d8925cf
Viktoryia Ventskute <ventskute.vika@gmail.com>
2021-02-04 00:59:33 +0300

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,14 @@ import startupModal from './components/startupModal';
 import { load, parseSave, setDefault } from './utils/saveSystem';
 import createGlobalListeners from './global/listeners';
 import disclaimer from './components/disclaimer';
-import Map from './pages/Map';
-import variables from './global/variables';
-import Level from './pages/Level';
-import levels from './configs/levels';
 
 !load() && setDefault();
 parseSave();
 
 createGlobalListeners();
 
-variables.currentLevel = 6;
-
 append([
-  Level(levels[6])
+  Menu(),
+  startupModal(),
+  disclaimer()
 ], $('#root'))
